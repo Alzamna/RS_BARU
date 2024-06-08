9fbac81a2e2f9dfa1228226e05c3bd4767c399fa
alzamna <alzamna195@gmail.com>
2024-06-08 19:05:43 +0700

    first commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Events\Validated;
+
 abstract class Controller
 {
-    //
+    public function getCount($tableName)
+{
+    global $conn;
+
+    $table = ($tableName);
+    $query = "SELECT * FROM $table";
+    $result = mysqli_query($conn, $query);
+    $totalCount = mysqli_num_rows($result);
+    return $totalCount;
+
+}
 }
